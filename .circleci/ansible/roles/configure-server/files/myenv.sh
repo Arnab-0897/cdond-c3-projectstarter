2cbcf4f26f84bbcc4890b6fb40a1127fd68930d4
Arnab-0897 <66132256+Arnab-0897@users.noreply.github.com>
2023-05-24 18:33:36 +0530

    Update myenv.sh

--- .circleci/ansible/roles/configure-server/files/myenv.sh
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -1,6 +1,6 @@
 export TYPEORM_CONNECTION=postgres
 export TYPEORM_ENTITIES=./scr/modules/domain/**/*.entity.ts 
-export TYPEORM_HOST=database-1.crmwpcvihtar.us-east-1.rds.amazonaws.com
+export TYPEORM_HOST=database-1.cdtbbingfx4k.us-east-1.rds.amazonaws.com
 export TYPEORM_PORT=5432
 export TYPEORM_USERNAME=postgres
 export TYPEORM_PASSWORD=postgres 
